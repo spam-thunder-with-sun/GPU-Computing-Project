65d5b48bc032f7ff7823a023261685c20b6181ee
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2023-07-07 11:54:45 +0200

    Aggiunta job.sh

---
+++ src/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=edu5
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:00
+#SBATCH --job-name=SATproject
+#SBATCH --output=out.txt
+#SBATCH --error=err.txt
+
+make build
+
+srun sat_to_matrix_mult
